5d16a45481b9a26c279494f6584f7cd496534bc7
Joel <joelcarrasco.sd@gmail.com>
2023-01-23 20:43:19 -0500

    some

---
+++ arraysJS/some/some.js
@@ -0,0 +1,13 @@
+const numbers = [
+  { paquete: "1", delivered: true },
+  { paquete: "2", delivered: true },
+  { paquete: "3", delivered: false },
+];
+// return a true si la condicion es cierta
+const par = numbers.some((item) => {
+  if (item.delivered == false) {
+    console.log("el paquete: ", item.paquete, " no fue entregado");
+  }
+});
+
+console.log("is par", par);
