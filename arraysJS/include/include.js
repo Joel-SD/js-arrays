79136e201dc732852989b400fab8dd0ad110e1be
Joel <joelcarrasco.sd@gmail.com>
2023-01-25 08:02:32 -0500

    include

--- arraysJS/include/include.js
+++ arraysJS/include/include.js
@@ -11,4 +11,14 @@ const array = ["tenis", "aroz", "arroz", "arbeja"];
 const newArray = array.filter((item) => {
   return item.includes("a");
 });
-console.log(newArray);
+/* console.log(newArray); */
+
+function filterByTerm(array, term) {
+  return array.filter((item) => {
+    item.includes(term);
+  });
+}
+const arra = ["ana", "santi", "nico", "anastasia"];
+const ter = "ana";
+
+console.log(filterByTerm(arra, ter));
